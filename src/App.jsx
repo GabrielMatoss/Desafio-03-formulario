94417ff76451c8940ea6a25d2f70cedc9b506269
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-01-16 09:15:19 -0300

    controlando os inputs

--- src/App.jsx
+++ src/App.jsx
@@ -35,39 +35,73 @@ Ao enviar, deve-se apresentar um alert javascript com sucesso, limpar todos os c
 do formulário e zerar a barra de progresso novamente.
 */
 
+import { useState } from "react";
+
 function App() {
+  const [data, setData] = useState({
+    fullName: "",
+    email: "",
+    maritalStatus: "",
+    genre: "",
+  });
+
+  function handleClick() {
+    console.log("changed");
+  }
   return (
-    <div className='App'>
+    <div className="App">
       <h3>desafio fernandev</h3>
       <h1>progresso do formulário</h1>
 
       <main>
         {/* crie a barra de progresso aqui */}
-        <div className='form-group'>
-          <label htmlFor=''>Nome Completo</label>
-          <input />
+        <div className="form-group">
+          <label htmlFor="">Nome Completo</label>
+          <input
+            name="fullName"
+            value={data.fullName}
+            onChange={handleChange}
+          />
         </div>
-        <div className='form-group'>
-          <label htmlFor=''>E-mail</label>
-          <input />
+        <div className="form-group">
+          <label htmlFor="">E-mail</label>
+          <input name="email" value={data.email} onChange={handleChange} />
         </div>
-        <div className='form-group'>
-          <label htmlFor=''>Estado Civil</label>
-          <select>
-            <option value=''>- selecione...</option>
-            <option value='solteiro'>Solteiro</option>
-            <option value='casado'>Casado</option>
-            <option value='divorciado'>Divorciado</option>
+        <div className="form-group">
+          <label htmlFor="">Estado Civil</label>
+          <select
+            name="maritalStatus"
+            value={data.maritalStatus}
+            onChange={handleChange}
+          >
+            <option value="">- selecione...</option>
+            <option value="solteiro">Solteiro</option>
+            <option value="casado">Casado</option>
+            <option value="divorciado">Divorciado</option>
           </select>
         </div>
-        <div className='form-group'>
-          <label htmlFor=''>Gênero</label>
-          <div className='radios-container'>
+        <div className="form-group">
+          <label htmlFor="">Gênero</label>
+          <div className="radios-container">
             <span>
-              <input type='radio' /> Masculino
+              <input
+                type="radio"
+                name="genre"
+                value="masculino"
+                onChange={handleChange}
+                checked={data.genre === "masculino"}
+              />{" "}
+              Masculino
             </span>
             <span>
-              <input type='radio' /> Feminino
+              <input
+                type="radio"
+                name="genre"
+                value="feminino"
+                onChange={handleChange}
+                checked={data.genre === "feminino"}
+              />{" "}
+              Feminino
             </span>
           </div>
         </div>
